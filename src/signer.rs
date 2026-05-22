4ffddc6c33599cfc153d1aa79b56974c4549bc7f
s1ashxd <s1ash.xd224@gmail.com>
2026-05-22 15:03:24 +0000

    add keypair signer

--- src/signer.rs
+++ src/signer.rs
@@ -1,7 +1,32 @@
+use ed25519_dalek::{Signer as DalekSigner, SigningKey};
 use solana_sdk::pubkey::Pubkey;
 
 pub trait Signer {
     fn pubkey(&self) -> Pubkey;
 
     fn sign(&self, message: &[u8]) -> [u8; 64];
+}
+
+pub struct KeypairSigner {
+    signing_key: SigningKey,
+    pubkey: Pubkey,
+}
+
+impl KeypairSigner {
+    #[must_use]
+    pub fn from_bytes(secret: &[u8; 32]) -> Self {
+        let signing_key = SigningKey::from_bytes(secret);
+        let pubkey = Pubkey::new_from_array(signing_key.verifying_key().to_bytes());
+        Self { signing_key, pubkey }
+    }
+}
+
+impl Signer for KeypairSigner {
+    fn pubkey(&self) -> Pubkey {
+        self.pubkey
+    }
+
+    fn sign(&self, message: &[u8]) -> [u8; 64] {
+        self.signing_key.sign(message).to_bytes()
+    }
 }
--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -7,3 +7,17 @@ fn stamper_error_variants_exist() {
     let e = StamperError::TransactionTooLarge { size: 1300 };
     assert!(format!("{e}").contains("1300"));
 }
+
+use ed25519_dalek::Verifier;
+use tx_stamper::signer::{KeypairSigner, Signer};
+
+#[test]
+fn keypair_signer_round_trip() {
+    let secret = [42u8; 32];
+    let signer = KeypairSigner::from_bytes(&secret);
+    let msg = b"hello world";
+    let sig = signer.sign(msg);
+    let vk = ed25519_dalek::VerifyingKey::from_bytes(&signer.pubkey().to_bytes()).unwrap();
+    let dalek_sig = ed25519_dalek::Signature::from_bytes(&sig);
+    assert!(vk.verify(msg, &dalek_sig).is_ok());
+}
